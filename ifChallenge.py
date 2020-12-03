e85a4a7c5dd08a32012e1d507fa4bdfe11cfda37
SwedishStig <75444230+SwedishStig@users.noreply.github.com>
2020-12-03 13:00:48 -0500

    Update ifChallenge.py

--- ifChallenge.py
+++ ifChallenge.py
@@ -4,4 +4,4 @@ age = int(input("How old are you? "))
 if 18 <= age <= 30:
     print(f"Welcome to the party {name}")
 else:
-    print("Pee is stored in the balls, g e t  f u c k e d")
+    print("Sorry, but you're not cool.")
