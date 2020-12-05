fbe8617b96c6401324a3c6ccfb22f9374ff6eeff
Larson <75444230+SwedishStig@users.noreply.github.com>
2020-12-05 15:56:56 -0500

    Add files via upload

---
+++ cave_game.py
@@ -0,0 +1,36 @@
+import shelve
+
+cave_make = shelve.open("cave")
+
+locations = cave_make["locations"]
+vocabulary = cave_make["vocabulary"]
+
+loc = 1
+while True:
+    availableExits = ", ".join(locations[loc]["exits"].keys())
+
+    print(locations[loc]["desc"])
+
+    if loc == 0:
+        break
+    else:
+        allExits = locations[loc]["exits"].copy()
+        allExits.update(locations[loc]["namedExits"])
+
+    direction = input("Available exits are " + availableExits).upper()
+    print()
+
+    # Parse the user input, using our vocabulary dictionary if necessary
+    if len(direction) > 1:  # more than 1 letter, so check vocab
+        words = direction.split()
+        for word in words:
+            if word in vocabulary:   # does it contain a word we know?
+                direction = vocabulary[word]
+                break
+
+    if direction in allExits:
+        loc = allExits[direction]
+    else:
+        print("You cannot go in that direction")
+
+cave_make.close()
